862943fd759298334f1f3218f45952bded1ee3cf
KL Lai <emf.gauss+git@gmail.com>
2025-07-06 16:07:23 -0700

    25-07-06 1605 Added Linux NAIF SPICE files. May not work, need to test from Matlab.

---
+++ helloSPICE2.m
@@ -0,0 +1,54 @@
+% The most basic demo of SPICE in Matlab
+clc; clear all; close all;
+
+project_root = fileparts(mfilename('fullpath'));  % Gets path to current script
+addpath(fullfile(project_root, 'base_unit'));
+addpath(fullfile(project_root, 'functions'));
+addpath(fullfile(project_root, 'functions', 'attitude'));
+addpath(fullfile(project_root, 'spice', 'kernels'));
+
+#addpath(fullfile('spice', 'mice', 'src', 'mice')); % addpath(fullfile('spice', 'mice', 'src', 'cspice')); % optional path
+#addpath(fullfile('spice', 'mice', 'lib'));
+
+addpath(fullfile(project_root, 'spice', 'mice_linux', 'src', 'mice'));
+addpath(fullfile(project_root, 'spice', 'mice_linux', 'lib'));
+
+
+% Load the necessary SPICE kernels
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'naif0012.tls'));  % Load a leapseconds kernel
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'de440s.bsp'));
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'pck00011.tpc'));
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'gm_de440.tpc'));
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'earth_200101_990827_predict.bpc'));
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'earth_assoc_itrf93.tf'));
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'moon_assoc_me.tf'));
+% cspice_furnsh(fullfile('spice', 'kernels', 'moon_assoc_pa.tf')); % only me or pa, not both
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'moon_de440_220930.tf'));
+cspice_furnsh(fullfile(project_root, 'spice', 'kernels', 'moon_pa_de440_200625.bpc'));
+
+% Time Conversion
+utc_time        = '2024-11-01T00:00:00';
+et_time         = cspice_str2et(utc_time);
+
+% Convert ephemeris time back to UTC string
+utc_time        = cspice_et2utc(et_time, 'C', 4);
+
+% Frames Transformation
+position_j2000          = [10000 15000 8000]';
+transformation_matrix   = cspice_pxform('J2000', 'ECLIPJ2000', et_time); % from, to, et
+position_ecliptic       = transformation_matrix * position_j2000;
+
+% Compute light time from Earth to Mars
+et_earth            = cspice_str2et('2024-01-01T00:00:00');
+et_mars             = et_earth + 3600;  % Assume one hour later
+[state_earth, ~]    = cspice_spkpos('EARTH', et_earth, 'J2000', 'NONE', 'SOLAR_SYSTEM_BARYCENTER');
+% [state_mars, ~]     = cspice_spkpos('MARS',  et_mars,  'J2000', 'NONE', 'SOLAR_SYSTEM_BARYCENTER'); % Need approproate kernels
+% light_time          = cspice_reclat(state_mars - state_earth);
+
+% Ephemeris
+[state_vector]      = cspice_spkezr('MOON', et_time, 'J2000', 'NONE', 'EARTH');
+position            = state_vector(1:3, 1);
+velocity            = state_vector(4:6, 1);
+
+% Clear Kernels
+% cspice_kclear; % it's good idea to clear SPICE from the system
